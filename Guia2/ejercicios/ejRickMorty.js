740f8a13b018a925070d477e6503aed18cb7a21a
MaximilianoDelfino <maximilianodelf2@gmail.com>
2026-04-22 06:33:39 -0300

    se anadio ejercicio 7

--- Guia2/ejercicios/ejRickMorty.js
+++ Guia2/ejercicios/ejRickMorty.js
@@ -49,14 +49,24 @@ fetch("https://rickandmortyapi.com/api/character")
 //     })
 // })
 
-.then(data => {
-    let ordenEpisodios = [...data.results].sort((a,b) => {
-        let episodios1 = a.episode.length;
-        let episodios2 = b.episode.length;
-        return episodios2 - episodios1;
-    });
-    ordenEpisodios.forEach(personajes => {
-        let cantEpisodios = personajes.episode.length;
-        document.getElementById("resultado").innerHTML += `<p>${personajes.name} aparece en ${cantEpisodios} episodios</p>`
-    })
+// .then (data => {
+//     let ordenEpisodios = [...data.results].sort((a,b) => {
+//         let episodios1 = a.episode.length;
+//         let episodios2 = b.episode.length;
+//         return episodios2 - episodios1;
+//     });
+//     ordenEpisodios.forEach(personajes => {
+//         let cantEpisodios = personajes.episode.length;
+//         document.getElementById("resultado").innerHTML += `<p>${personajes.name} aparece en ${cantEpisodios} episodios</p>`
+//     })
+// })
+
+.then (data => {
+    let tieneImagen = (imagen) => (imagen.image) !== "";
+    let imagenes = data.results.every(tieneImagen);
+    if (imagenes === true) {
+        document.getElementById("resultado").innerHTML += `<p>✅ Todos los personajes tienen imagen</p>` 
+    }else{   
+        document.getElementById("resultado").innerHTML += `<p>⚠️ Hay personajes sin imagen</p>` 
+    };
 })
